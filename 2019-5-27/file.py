78092dbee5c5d8f392489040e8ea67d39ac4d9cc
miyunke <miyunke@iauto.com>
2019-05-27 19:52:58 +0800

    add 2019-5-27

--- 2019-5-27/file.py
+++ 2019-5-27/file.py
@@ -1,6 +1,8 @@
 fout = open('outpot.txt', 'w')
-line1 = "This here's the wattle.\n"
-line2 = "the emblem of our land.\n"
-fout.write(line1)
-print(fout.write(line2))
+#line1 = "This here's the wattle.\n"
+#line2 = "the emblem of our land.\n"
+#fout.write(line1)
+#print(fout.write(line2))
+x = 52
+fout.write(str(x))  #write参数必须是字符串
 fout.close()
